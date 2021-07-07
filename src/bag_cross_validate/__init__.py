d85fd3640bc0765cf4e2055809dc0f26918478a2
John Vorsten <vorstenjohn@gmail.com>
2021-07-07 18:45:30 -0500

    add automatic import

---
+++ src/bag_cross_validate/__init__.py
@@ -0,0 +1,5 @@
+from .bag_cross_validate import cross_validate_bag, bags_2_si, bags_2_si_generator, BagScorer
+
+__author__ = 'John Vorsten'
+__email__ = 'vorstenjohn@gmail.com'
+__version__ = '0.0.1'
